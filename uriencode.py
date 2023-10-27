dfd459386e3627e3b61822c3f4ba609267a35deb
sar1n <github@heynina.me>
2023-10-27 04:09:09 -0400

    Refactors code to use arguments instead of menu; adds ability to force encoding of all characters

--- uriencode.py
+++ uriencode.py
@@ -1,25 +1,31 @@
-import urllib.parse
+import urllib.parse as urlparse
+import argparse
 
-string_to_encode = input("Enter string to encode: ")
-pass_count = int(input("Encode how many times (default: 1)? ") or 1)
+parser = argparse.ArgumentParser(description='uri encode a given string one or more times')
+parser.add_argument('-s', '--string', help='The string to be uri-encoded')
+parser.add_argument('-c', '--count', default=1, help='Number of times to uri-encode the given string (default: 1)')
+parser.add_argument('-a', '--all', action='store_true', help='Force encoding of ALL characters (/ and . included)')
+args = parser.parse_args()
 
 
 def encode():
-    print('** ** ** ** **')
-    print("** encoding **")
-    print('** ** ** ** **')
-
     # make the initial encoding pass and reduce passes by 1
-    passes = pass_count
-    encoded_string = urllib.parse.quote(string_to_encode)
+    passes = int(args.count)
+
+    if args.all:
+        encoded_string = urlparse.quote(args.string, safe='')
+        encoded_string = encoded_string.replace('.', '%2E')
+    else:
+        encoded_string = urlparse.quote(args.string)
+
     passes -= 1
 
     # if there are passes remaining, encode again
     if passes:
         for x in range(passes):
-            encoded_string = urllib.parse.quote(encoded_string)
+            encoded_string = urlparse.quote(encoded_string)
 
-    print('Encoded string: ')
+    print('Encoded string:')
     print(encoded_string)
 
 
